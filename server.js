b8ce222efb36aebe67b971fcfefa04dc9185df60
Andres_Coronado <andrescoronadob5@gmail.com>
2021-10-26 00:05:19 +1100

    cambios

---
+++ server.js
@@ -0,0 +1,42 @@
+
+require('dotenv').config({path:"./config.env"})
+
+const express =  require('express');
+var MongoDBUtil = require('./config/mongodb/modules/mongodb/mongodb.module').MongoDBUtil;
+const errorHandler=require('./middleware/error');
+const createRoles=require('./lib/inicioSetup')
+var logger = require('morgan');
+const cors=require("cors");
+
+
+
+ // Use this after the variable declaration
+////connections
+MongoDBUtil.init();
+
+var app=express();
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
+app.use(logger('dev'));
+createRoles();
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/private', require('./routes/private'))
+app.use('/api/home', require('./routes/home'))
+app.use('/api/products', require('./routes/products'))
+app.use('/api/ventas', require('./routes/ventas'))
+
+////error handler last middleware
+app.use(errorHandler)
+
+//const PORT= process.env.PORT || 3001;
+
+//app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
+
+module.exports= app;
